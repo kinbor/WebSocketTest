4275a5a30b9b70d6a56f03fc059211b1dc814db2
kinbor <kinbor@163.com>
2020-07-02 15:11:28 +0800

    code

---
+++ WebSocketTest.Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocket4Net;
+
+namespace WebSocketTest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("正在连接服务器。。。");
+            var ws = new WebSocket("ws://127.0.0.1:9090");
+            ws.Opened += Ws_Opened;
+            ws.Closed += Ws_Closed;
+            ws.MessageReceived += Ws_MessageReceived;
+            ws.Error += Ws_Error;
+            ws.Open();
+
+            Console.WriteLine("输入STOP后,按Enter键结束。。。");
+            while (true)
+            {
+                var info = Console.ReadLine();
+                if (info == "STOP")
+                {
+                    break;
+                }
+                else
+                {
+                    ws.Send(info);
+                }
+            }
+
+            ws.Close();
+            ws.Dispose();
+        }
+
+        private static void Ws_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            Console.WriteLine($"服务器：{e.Message}");
+        }
+
+        private static void Ws_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+        {
+            Console.WriteLine($"错误信息：{e.Exception.Message} | {e.Exception.StackTrace}");
+        }
+
+        private static void Ws_Closed(object sender, EventArgs e)
+        {
+            Console.WriteLine("服务器连接断开！");
+        }
+        private static void Ws_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("服务器连接成功！");
+        }
+    }
+}
---
+++ WebSocketTest.Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketTest.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //客户端url以及其对应的Socket对象字典
+            var dic_Sockets = new Dictionary<string, IWebSocketConnection>();
+            //创建
+            //本地测试的配置IP和端口，自行设置
+            WebSocketServer server = new WebSocketServer("ws://0.0.0.0:9090");
+            //出错后进行重启
+            server.RestartAfterListenError = true;
+
+            //开始监听
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>   //连接建立事件
+                {
+                    //获取客户端网页的url
+                    string clientUrl = socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort;
+                    dic_Sockets.Add(clientUrl, socket);
+                    Console.WriteLine(DateTime.Now.ToString() + "|服务器:和客户端网页:" + clientUrl + " 建立WebSock连接！");
+                };
+                socket.OnClose = () =>  //连接关闭事件
+                {
+                    string clientUrl = socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort;
+                    //如果存在这个客户端,那么对这个socket进行移除
+                    if (dic_Sockets.ContainsKey(clientUrl))
+                    {
+                        //注:Fleck中有释放
+                        //关闭对象连接
+                        //if (dic_Sockets[clientUrl] != null)
+                        //{
+                        //dic_Sockets[clientUrl].Close();
+                        //}
+                        dic_Sockets.Remove(clientUrl);
+                    }
+                    Console.WriteLine(DateTime.Now.ToString() + "|服务器:和客户端网页:" + clientUrl + " 断开WebSock连接！");
+                };
+                socket.OnMessage = message =>  //接受客户端网页消息事件
+                {
+                    string clientUrl = socket.ConnectionInfo.ClientIpAddress + ":" + socket.ConnectionInfo.ClientPort;
+                    //socket.ConnectionInfo.Headers["UserId"] = message;
+                    string key = message.Replace("UserId=", "");
+                    dic_Sockets.Add(key, socket);
+                    Console.WriteLine(DateTime.Now.ToString() + "|服务器:【收到】来客户端网页:" + clientUrl + "的信息：\n" + message);
+                };
+            });
+
+            Console.ReadKey();
+            foreach (var item in dic_Sockets.Values)
+            {
+                if (item.IsAvailable == true)
+                {
+                    item.Send("服务器消息：" + DateTime.Now.ToString());
+                }
+            }
+
+            //关闭与客户端的所有的连接
+            Console.ReadKey();
+            foreach (var item in dic_Sockets.Values)
+            {
+                if (item != null)
+                {
+                    item.Close();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
